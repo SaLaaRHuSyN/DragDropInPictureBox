f6c4428d7afa9531b152d21bc3ad9f36ab305e53
SaLaaR HuSyN <shsallu35@gmail.com>
2018-12-07 16:34:30 +0500

    Initial commit

---
+++ DragDropInPictureBox/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragDropInPictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string pic in ((string[])e.Data.GetData(DataFormats.FileDrop)))
+            {
+                Image img = Image.FromFile(pic);
+                pictureBox1.Image = img;
+            }
+        }
+    }
+}
